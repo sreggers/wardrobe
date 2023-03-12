680d07da3f02d66b858f3e4dd6bc048a52cef1fd
sreggers <sebastiaan@reggers.org>
2023-03-12 13:58:40 +0100

    initial commit

---
+++ src/main/java/be/kwispel/wardrobe/WardrobeApplication.java
@@ -0,0 +1,13 @@
+package be.kwispel.wardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/kwispel/wardrobe/WardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kwispel.wardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
